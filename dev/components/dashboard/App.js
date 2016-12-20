a466f2c86a31eca0bfb86a7cef98abfc868f989d
Jimmy Gonzalez <jimmygonzalez87@gmail.com>
2016-12-20 15:15:51 -0800

    [setup] added routes and aframe

---
+++ dev/components/dashboard/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+
+
+
+class App extends Component {
+  render() {
+    // let children = null;
+    // if (this.props.children) {
+    //   children = React.cloneElement(this.props.children, {
+    //     test: "something" //sends auth instance from route to children
+    //   })
+    // }
+    return (
+      <div  class="container-fluid">
+       dashboard
+
+      </div>
+    );
+  }
+}
+
+export default App;
