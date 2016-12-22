7de544df1e031986da00b96fb9b37f3a46e9aefc
Kevin Arthur <kevin.arthur.roy@gmail.com>
2016-12-22 14:22:47 -0800

    Update index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../user/UserController');
-const tagController = require('../tag/TagController');
-const postController = require('../post/PostController');
+const userController = require('../db/user/UserController');
+const postController = require('../db/post/PostController');
+const followController = require('../db/follow/FollowController.js');
 //USER CONTROLLER routes================>
 //get one user
 router.get('/users/:userId', userController.getOneUser);//MVP
@@ -17,30 +17,30 @@ router.get('/tags/:tagId/users', userController.getUsersWithTag);
 
 //POSTS CONTROLLER routes================>
 //get one post
-router.get('/posts/:postId', TODO);//MVP
+router.get('/posts/:postId', postController.getOnePost);//MVP
 //get all posts
-router.get('/posts', TODO);//MVP
+router.get('/posts', postController.getAllPosts);//MVP
 //get posts by user
-router.get('/users/:userId/posts', TODO); //MVP
+router.get('/users/:userId/posts', postController.getPostsByUser); //MVP
 //get posts by tag
-router.get('/tags/:tagId/posts', TODO);
+router.get('/tags/:tagId/posts', postController.getPostsByTag);
 //create post
-router.post('/posts', TODO);//MVP
+router.post('/posts', postController.createPost);//MVP
 //update post
-router.put('/posts/:postId', TODO);
+router.put('/posts/:postId', postController.updatePost);
 //respond to post
-router.post('/posts/:postId', TODO);
+router.post('/posts/:postId', postController.respondToPost);
 
 //FOLLOWS CONTROLLER routes================>
 //get followers for user
-router.get('/users/:userId/followers', TODO);//MVP
+router.get('/users/:userId/followers', followController.getFollowersForUser);//MVP
 //request follow
-router.post('/users/:userId/follows', TODO);//MVP
+router.post('/users/:userId/follows', followController.requestFollow);//MVP
 //accept follow
-router.put('/users/:userId/follows/:followId', TODO);
+router.put('/users/:userId/follows/:followId', followController.acceptFollow);
 //delete follow
-router.delete('/users/:userId/follows/:followId', TODO);
+router.delete('/users/:userId/follows/:followId', followController.deleteFollow);
 //get followed by user
-router.get('/users/:userId/follows');
+router.get('/users/:userId/follows', followController.getFollowedByUser);
 
 module.exports = router;
