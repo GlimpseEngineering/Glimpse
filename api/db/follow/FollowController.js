704038c8a424f00b1721a4020281a8e01f8e4c34
Kevin Arthur <kevin.arthur.roy@gmail.com>
2016-12-22 12:31:31 -0800

    [setup] requestFollow fx working

---
+++ api/db/follow/FollowController.js
@@ -0,0 +1,99 @@
+const models = require('../../config/db.connect.js');
+
+module.exports = {
+  getFollowersForUser: (req, res, next) => {
+    // models.Follow.findAll({
+
+    // })
+    // .then(result => {
+    //   res.json(result);
+    // })
+    // .catch(err => {
+
+    // })
+  },
+
+  requestFollow: (req, res, next) => {
+    models.Follow.findOrCreate({
+      where: {
+        UserId: req.params.userId,
+        FollowId: req.params.followId,
+        status: 'pending'
+      },
+      defaults: {
+        UserId: req.params.userId,
+        FollowId: req.params.followId,
+        status: 'pending'
+      }
+    })
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.json(err);
+      throw err;
+    });
+  },
+
+  acceptFollow: (req, res, next) => {
+    models.Follow.findOne({
+      where: {
+        UserId: req.params.userId,
+        FollowId: req.params.followId,
+        status: 'pending'
+      }
+    })
+    .then(result => {
+      if (result) {
+        result.status = 'accepted'
+        return result.save();
+      } else {
+        res.json([]);
+      }
+    })
+    .then(result => {
+      res.json([result]);
+    })
+    .catch(err => {
+      res.json(err);
+      throw err;
+    });
+  },
+
+  deleteFollow: (req, res, next) => {
+    models.Follow.findOne({
+      where: {
+        UserId: req.params.userId,
+        FollowId: req.params.followId,
+        status: 'accepted'
+      }
+    })
+    .then(result => {
+      return result.destroy();
+    })
+    .then(result => {
+      res.json([result])
+    })
+    .catch(err => {
+      res.json(err);
+      throw err;
+    });
+  },
+
+  getFollowedByUser: (req, res, next) => {
+    models.Follow.findAll({
+      where: {
+        UserId: req.params.userId,
+        status: 'accepted'
+      }
+    })
+    .then(result => {
+      if (Array.isArray(result)) res.json(result);
+      else res.json([result]);
+    })
+    .catch(err => {
+      res.json(err);
+      throw err;
+    });
+  }
+};
