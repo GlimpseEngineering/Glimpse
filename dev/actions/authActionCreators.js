83f53b6b82e94dd9c19853539ebf3ea37a954434
Nathaniel Hart <Nathaniel Hart>
2016-12-28 15:14:01 -0800

    [prog] working on reimplementing authentication after git ate everything

---
+++ dev/actions/authActionCreators.js
@@ -0,0 +1,45 @@
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_ERROR = 'LOGIN_ERROR'
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
+
+function loginSuccess(profile) {
+  return {
+    type: LOGIN_SUCCESS,
+    profile
+  }
+}
+
+function loginError(error) {
+  return {
+    type: LOGIN_ERROR,
+    error
+  }
+}
+
+function logoutSuccess(profile) {
+  return {
+    type: LOGOUT_SUCCESS
+  }
+}
+
+export function login() {
+  const lock = new Auth0Lock(process.env.AUTH_CLIENT_ID, process.env.AUTH_DOMAIN)
+  return dispatch => {
+    lock.show((error, profile, token) => {
+      if(error) {
+        return dispatch(loginError(error))
+      }
+      localStorage.setItem('profile', JSON.stringify(profile))
+      localStorage.setItem('id_token', token)
+      return dispatch(loginSuccess(profile))
+    })
+  }
+}
+
+export function logout() {
+  return dispatch => {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('profile');
+    return dispatch(logoutSuccess());
+  }
+}
