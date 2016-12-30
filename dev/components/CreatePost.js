e0b68cf9baa9bc1544072e4b47369a22a48195f9
Kevin Arthur <kevin.arthur.roy@gmail.com>
2016-12-30 13:31:29 -0800

    [prog] creating forms for primitives

--- dev/components/CreatePost.js
+++ dev/components/CreatePost.js
@@ -24,6 +24,14 @@ class CreatePost extends Component {
       private: 0,
       tags: ''
     };
+
+    this.primitiveCollection = [];
+
+    this.basePrimitive = {
+      primitive: '',
+      components: {},
+      children: null
+    };
   }
 
   submitPost(event) {
@@ -35,65 +43,79 @@ class CreatePost extends Component {
     });
   }
 
+  submitScene(event) {
+    event.preventDefault();
+  }
+
   onInputChange(event) {
     event.preventDefault();
     let name = event.target.name;
     let value = event.target.value;
-    name === 'content' && this.setState({content: value});
     name === 'description' && this.setState({description: value});
     name === 'private' && this.setState({private: value});
     name === 'tags' && this.setState({tags: value});
+    name === 'primitive' && console.log(value);
   };
 
   render() {
     return (
-      <form onSubmit={this.submitPost.bind(this)}>
+      <div>
         <h3>Create A New Scene</h3>
-
-        <select>
+          
+        <select 
+          name="primitive"
+          onChange={event => this.onInputChange(event)} >
           <option value="PhotoSphere">PhotoSphere</option>
           <option value="Camera">Camera</option>
           <option value="Text">Text</option>
         </select>
 
-        <div>
-          <label>Content</label>
-          <input 
-            type="text"
-            name="content"
-            value={this.state.content}
-            onChange={event => this.onInputChange(event)} />
-        </div>
+        <form 
+          id="photosphere"
+          onSubmit={this.submitScene.bind(this)} >
+          <div>
+            <label>Image URL</label>
+            <input 
+              type="text"
+              name="url" />
+          </div>
+          
+          <button type="submit">Add this scene!</button>
+        </form>
+        
+        <form 
+          id="post"
+          onSubmit={this.submitPost.bind(this)} >
+          <div>
+            <label>Description</label>
+            <textarea
+              name="description"
+              value={this.state.description}
+              onChange={event => this.onInputChange(event)} >
+            </textarea>
+          </div>
 
-        <div>
-          <label>Description</label>
-          <textarea
-            name="description"
-            value={this.state.description}
-            onChange={event => this.onInputChange(event)} >
-          </textarea>
-        </div>
+          <div>
+            <label>Make Private</label>
+            <input 
+              type="radio" 
+              name="private"
+              value={this.state.private}
+              onChange={event => this.onInputChange(event)} />
+          </div>
 
-        <div>
-          <label>Make Private</label>
-          <input 
-            type="radio" 
-            name="private"
-            value={this.state.private}
-            onChange={event => this.onInputChange(event)} />
-        </div>
+          <div>
+            <label>Tags</label>
+            <input 
+              type="text"
+              name="tags"
+              value={this.state.tags}
+              onChange={event => this.onInputChange(event)} />
+          </div>
 
-        <div>
-          <label>Tags</label>
-          <input 
-            type="text"
-            name="tags"
-            value={this.state.tags}
-            onChange={event => this.onInputChange(event)} />
-        </div>
-
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     );
   }
 };
