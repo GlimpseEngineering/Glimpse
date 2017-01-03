ad23d98191ba2d5c2244683e79a012d57f2707f4
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-01-03 09:04:58 -0800

    [prog] able to click on user and make request

--- dev/actions/searchActionCreators.js
+++ dev/actions/searchActionCreators.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const CLEAR_SEARCH = 'CLEAR_SEARCH';
 export const SEARCH_USERS = 'SEARCH_USERS';
+export const FOLLOW_FOUND_USER = 'FOLLOW_FOUND_USER';
 
 export function searchUser(searchterm, UserId) {
   const request = axios.get(`api/search/users/${UserId}/${searchterm}`);
@@ -9,7 +10,7 @@ export function searchUser(searchterm, UserId) {
   return (dispatch) => {
     request
       .then((foundUsers) => {
-        dispatch({ type: SEARCH_USERS, payload: foundUsers.data})
+        dispatch({ type: SEARCH_USERS, payload: foundUsers.data });
       })
       .catch((error) => {
         throw error;
@@ -20,5 +21,20 @@ export function searchUser(searchterm, UserId) {
 export function clearSearch() {
   return (dispatch) => {
     dispatch({ type: CLEAR_SEARCH });
+  };
+};
+
+export function followFoundUser(userId, followId) {
+  const request = axios.post(`api/users/${userId}/follows/${followId}`);
+
+  return (dispatch) => {
+    request
+      .then((followRequest) => {
+        console.log('Here is our request to follow an individual', followRequest.data);
+        dispatch({ type: FOLLOW_FOUND_USER, payload: followRequest.data })
+      })
+      .catch((error) => {
+        throw error;
+      });
   };
 };
--- dev/components/search/SearchResult.js
+++ dev/components/search/SearchResult.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import { followFoundUser } from '../../actions/searchActionCreators';
 
 class SearchResult extends Component {
   constructor(props) {
@@ -11,28 +12,30 @@ class SearchResult extends Component {
     };
   }
 
+  /** may need to change this lifecycle hook */
   componentDidMount() {
     let followRequest = this.props.foundUsers.followedByUser[this.props.searchResult.id];
     followRequest && followRequest.status === 'pending' && this.setState({button: 'pending'});
     followRequest && followRequest.status === 'accepted' && this.setState({button: 'following'});
     /**
-     * need to dispatch an action when calling requestFollow
-     * need to dispatch an action when calling acceptFollow
-     * 
-     * trying to check and see if the user is already following the searched individual (make call to getFollowByUser in controller)
-     * if record already exists in the follows table, and they are following, can update the button
-     * 
-     * otherwise, clicking follow just calls the requestFollow f(x) in the controller
-     * for the moment: can also call acceptFollow since we are ignoring privacy atm
+     * need to dispatch an action that calls requestFollow -> returned model instance should be stored in state
+     * need to dispatch an action that calls acceptFollow -> if accepted, existing model instance in state should be updated
      * 
      */
+  }
+
+  onClick(event) {
+    event.preventDefault();
+    console.log('followId', this.props.searchResult.id);
+    console.log('userId', this.props.auth.activeUser.id);
+    this.props.followFoundUser(this.props.auth.activeUser.id, this.props.searchResult.id);
   }
 
   render() {
     return (
       <div>
         {this.props.searchResult.username}
-        <button>{this.state.button}</button>
+        <button onClick={this.onClick.bind(this)}>{this.state.button}</button>
       </div>
     );
   }
@@ -45,5 +48,5 @@ function mapStateToProps(state){
   };
 }
 
-const searchResult = connect(mapStateToProps, null)(SearchResult);
+const searchResult = connect(mapStateToProps, { followFoundUser })(SearchResult);
 export default searchResult;
