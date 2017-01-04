ff5c70f3dc6215f570b4024d1454ab986083d230
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-01-03 16:40:07 -0800

    [prog] updating follow status on render

--- dev/components/search/SearchResult.js
+++ dev/components/search/SearchResult.js
@@ -12,36 +12,24 @@ class SearchResult extends Component {
     };
   }
 
-  /** 
-   * need to change this life cycle hook
-   * props are changing but component isn't mounting again, need it to change so searchResult can re-render to set state
-   */
-  // componentDidMount() {
-  //   let followRequest = this.props.foundUsers.followedByUser[this.props.searchResult.id];
-  //   followRequest && followRequest.status === 'pending' && this.setState({button: 'pending'});
-  //   followRequest && followRequest.status === 'accepted' && this.setState({button: 'following'});
-    /**
-     * need to dispatch an action that calls requestFollow -> returned model instance should be stored in state
-     * need to dispatch an action that calls acceptFollow -> if accepted, existing model instance in state should be updated
-     * 
-     */
-  // }
+  componentDidMount() {
+    let followRequest = this.props.foundUsers.followedByUser[this.props.searchResult.id];
+    followRequest && followRequest.status === 'pending' && this.setState({button: 'pending'});
+    followRequest && followRequest.status === 'accepted' && this.setState({button: 'following'});
+  }
 
+  /**
+   * need to dispatch an action that calls requestFollow -> returned model instance should be stored in state (done)
+   * need to dispatch an action that calls acceptFollow -> if accepted, existing model instance in state should be updated
+   */
   componentWillReceiveProps(nextProps) {
-    console.log(`clicked on user ${this.props.searchResult.id} with a current request status of ${this.props.foundUsers.followedByUser[this.props.searchResult.id]} vs a next request status of ${nextProps.foundUsers.followedByUser[nextProps.searchResult.id]}`)
-    // let followRequest = this.props.foundUsers.followedByUser[this.props.searchResult.id];
     let updatedRequest = nextProps.foundUsers.followedByUser[nextProps.searchResult.id];
-    console.log('updated props with follow request', updatedRequest);
-    // followRequest && followRequest.status === 'pending' && this.setState({button: 'pending'});
-    // followRequest && followRequest.status === 'accepted' && this.setState({button: 'following'});
     updatedRequest && updatedRequest.status === 'pending' && this.setState({button: 'pending'});
     updatedRequest && updatedRequest.status === 'accepted' && this.setState({button: 'following'});
   }
 
   onClick(event) {
     event.preventDefault();
-    // console.log('click event user', event.target.value);
-    console.log('click event user', this.props.searchResult.id);
     this.props.followFoundUser(this.props.auth.activeUser.id, this.props.searchResult.id);
   }
 
