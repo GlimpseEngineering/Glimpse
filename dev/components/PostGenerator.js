2d9bab0b1d3e13869bcff5949cbb96400079711a
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-01-04 12:26:25 -0800

    [prog] devised plan to interactively generate posts

--- dev/components/PostGenerator.js
+++ dev/components/PostGenerator.js
@@ -20,40 +20,41 @@ class PostGenerator extends Component {
 
     this.state = {
       userId: this.props.auth.activeUser.id,
-      content: '',
+      content: [],
       description: '',
       private: 0,
       tags: '',
-      src: '',
       sceneComplete: false
     };
 
     this.primitiveCollection = [];
-
-    this.basePrimitive = {
-      primitive: 'PhotoSphere',
-      components: {
-        src: ''
-      },
-      children: null
-    };
   }
 
   submitPost(event) {
     event.preventDefault();
     this.props.createPost(this.state);
-    console.log('user ' + this.props.auth.activeUser.id + ' made a post!!!!!');
     this.setState({
+      content: [],
       description: '',
-      tags: ''
+      private: 0,
+      tags: '',
+      sceneComplete: true
     });
   }
 
   submitScene(event) {
     event.preventDefault();
-    this.basePrimitive.components.src = this.state.src;
-    this.primitiveCollection.push(this.basePrimitive);
-    this.setState({content: JSON.stringify(this.primitiveCollection)});
+    /**
+     * going to have figure a way to manipulate data passed in to the store using this function
+     * can use the store to simply pass around an object
+     * initial state can just be an entity with a value of null
+     * replace the entity value with an object containing the requisite properties
+     * pass that value back to PostGenerator via props
+     * once back in props can make a copy of it, manipulate it however necessary
+     * then put it into the primitiveCollection in the correct format
+     * alternatively, we can give each entity a key that we can use for when we want to edit a scene
+     * finally, right before actual submission, we iterate through the object and pass the data to primitiveCollection in the correct format 
+     */
   }
 
   onInputChange(event) {
@@ -63,10 +64,13 @@ class PostGenerator extends Component {
     name === 'description' && this.setState({description: value});
     name === 'private' && this.setState({private: value});
     name === 'tags' && this.setState({tags: value});
-    name === 'url' && this.setState({src: value});
   }
 
   onPrimitiveChange(event) {
+    /**
+     * here we would want to load the various forms so that the user can edit them 
+     * so on change we can set a different form as visible 
+     */
     event.preventDefault();
     let name = event.target.name;
     let value = event.target.value;
@@ -74,7 +78,6 @@ class PostGenerator extends Component {
   }
 
   render() {
-    // console.log('inside of PostGenerator', this.props);
     return (
       <div>
         <h3>Create A New Scene</h3>
