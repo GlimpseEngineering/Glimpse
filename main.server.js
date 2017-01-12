86f60b320b455e4ef61672b276e6c32388ccf37d
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-01-11 16:16:58 -0800

    [fix] setting server based on  prod env var

--- main.server.js
+++ main.server.js
@@ -1,15 +1,20 @@
-const appServer = require('./webpack.server.js');
 const apiServer = require('./api.server.js');
 require('dotenv').config();
 const db = require('./api/config/db.connect.js');
 
 const PORT = process.env.PORT || 8080;
+const PROD = process.env.NODE_ENV === 'production';
 
 db.sequelize
-  .sync(/*{force: true}*/)
+  .sync()
   .then(() => {
-    apiServer(PORT - 1);
-    appServer(PORT);
+    if (PROD) {
+      apiServer(PORT);
+    } else {
+      const appServer = require('./webpack.server.js');
+      apiServer(PORT - 1);
+      appServer(PORT);
+    }
   })
   .catch((err) => {
     throw err;
