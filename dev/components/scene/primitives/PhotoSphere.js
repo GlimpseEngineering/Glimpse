d58ce6c9f87c1b300735f15ed654bca6ffeaebcd
Nathaniel Hart <Nathaniel Hart>
2017-01-13 09:29:33 -0800

    [fix] video audio issue and sphere gray out issue

--- dev/components/scene/primitives/PhotoSphere.js
+++ dev/components/scene/primitives/PhotoSphere.js
@@ -39,6 +39,7 @@ export default class PhotoShpere extends Component {
       this.format='img';
       this.shader=this.props.shader||'flat';
     } else if (this.props.src.endsWith('.mpg') ||
+               this.props.src.endsWith('.webm') ||
                this.props.src.endsWith('.mov') ||
                this.props.src.endsWith('.mp4') ||
                this.props.src.endsWith('.avi') ){
@@ -73,52 +74,54 @@ export default class PhotoShpere extends Component {
   }
 
   componentWillReceiveProps(nextProps){
-    this.setState({loading:true, shader:'gif'})
-    this.src=nextProps.src
-    if (this.src.endsWith(')')) {
-      this.src=this.src.slice(4,-1)
+    if (nextProps.src !== this.props.src) {
+      this.setState({loading:true, shader:'gif'})
+      this.src=nextProps.src
+      if (this.src.endsWith(')')) {
+        this.src=this.src.slice(4,-1)
+      }
+      console.log('this.props.src in photosphere:', this.props, typeof this.props.src)
+      console.log('nextProps.src in photosphere:', nextProps, typeof nextProps.src)
+      if (this.src.endsWith('.gif')) {
+        this.format='img';
+        this.shader='gif';
+      } else if (this.src.endsWith('.jpg') ||
+                this.src.endsWith('.png') ){
+        this.format='img';
+        this.shader=nextProps.shader||'flat';
+      } else if (this.src.endsWith('.mpg') ||
+                this.src.endsWith('.mov') ||
+                this.src.endsWith('.webm') ||
+                this.src.endsWith('.mp4') ||
+                this.src.endsWith('.avi') ){
+        this.format='video';
+        this.shader=nextProps.shader||'flat';
+      } else {
+        this.format='img';
+      }
+
+      let assetId=this.guid();
+
+      let assets = document.querySelector('a-assets')
+
+      console.log('assets at will receive props',assets)
+
+      let oldAsset = document.querySelector(`#${this.state.assetId}`)
+      console.log('old asset to be removed:',oldAsset)
+      oldAsset && document.querySelector('#assets').removeChild(oldAsset)
+      let asset = document.createElement(this.format)
+      asset.setAttribute('id',assetId)
+      asset.setAttribute('src',this.src)
+      console.log('new asset:',asset)    
+      assets.appendChild(asset)
+      console.log('assets with new asset:',assets.fileLoader.proto);
+
+      this.setState({
+        format: this.format,
+        shader: this.shader,
+        assetId: assetId
+      })
     }
-    console.log('this.props.src in photosphere:', this.props, typeof this.props.src)
-    console.log('nextProps.src in photosphere:', nextProps, typeof nextProps.src)
-    if (this.src.endsWith('.gif')) {
-      this.format='img';
-      this.shader='gif';
-    } else if (this.src.endsWith('.jpg') ||
-              this.src.endsWith('.png') ){
-      this.format='img';
-      this.shader=nextProps.shader||'flat';
-    } else if (this.src.endsWith('.mpg') ||
-              this.src.endsWith('.mov') ||
-              this.src.endsWith('.mp4') ||
-              this.src.endsWith('.avi') ){
-      this.format='video';
-      this.shader=nextProps.shader||'flat';
-    } else {
-      this.format='img';
-    }
-
-    let assetId=this.guid();
-
-    let assets = document.querySelector('a-assets')
-
-    console.log('assets at will receive props',assets)
-
-    let oldAsset = document.querySelector(`#${this.state.assetId}`)
-    console.log('old asset to be removed:',oldAsset)
-    oldAsset && document.querySelector('#assets').removeChild(oldAsset)
-    let asset = document.createElement(this.format)
-    asset.setAttribute('id',assetId)
-    asset.setAttribute('src',this.src)
-    console.log('new asset:',asset)    
-    assets.appendChild(asset)
-    console.log('assets with new asset:',assets.fileLoader.proto);
-
-    this.setState({
-      format: this.format,
-      shader: this.shader,
-      assetId: assetId
-    })
-
   }
 
   guid () {
